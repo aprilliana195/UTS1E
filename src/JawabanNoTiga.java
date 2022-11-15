c4ca1796df86d2dff88fa634e16fb248b193e415
Lenovo <aprilianapramesti37@gmail.com>
2022-11-15 13:55:51 +0700

    oke lagi

--- src/JawabanNoTiga.java
+++ src/JawabanNoTiga.java
@@ -1,5 +1,49 @@
-public class JawabanNoTiga {
-    public static void main(String[] args) {
-        System.out.println("helo 1e");
-    }
-}
+
+uts_22090103_apriliana niken pramesti_1E
+LINK GITHUB:Apriliananiken
+Jawaban no1:
+Apabila tipe data double dikalikan dengan tipe data float yang terjadi adalah float akan bertambah
+float nilai;
+f nilai;89,7f;
+console.write (f nilai)
+double nilai;88,5
+console.write (d nilai)
+char() nama c nama=new char
+nama [0]="a" 90
+nama [1]="b" 40
+nama [2]="c" 60
+nama [3]="d" 100
+int nilai 1,nilai 2,nilai 3,nilai 4
+#include <iostream>
+using namespace
+int main()
+double var 1= 4.75
+double var 2= 9.40
+double var 3= 1.00
+
+jawaban nomor 2
+public static void (string[]args){
+        float[]my number=new float[]{2.34f,5.56f,67.9f};
+        pembulatan keatas(number);
+        }
+
+        jawaban nomor 3
+string { info bus }
+public static doublefloat{
+public static void main(string[]args){
+        float[]mynumber=new float[]{2.34f,4.5of,67.89f};
+        var hasil1:double=math.ceil(2.34f);
+        Var hasil2:double=math.ceil(4.56f);
+        var hasil3:double=math.ceil(67.89f);
+
+        system.out.println("hasil 1= "+hasil1);
+        system.out.println("hasil 1="+hasil2);
+        system.out.println("hasil 1="=+hasil3);
+
+        }
+        }
+
+
+
+
+
